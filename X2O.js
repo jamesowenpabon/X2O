80aff908c6f58bb66ece493aecb9207b233d69aa
James <jamesowenpabon@gmail.com>
2017-09-28 15:25:55 -0400

    Added multiple constructor support and function support.

--- X2O.js
+++ X2O.js
@@ -1,22 +1,29 @@
 var X2O = X2O || {};
 
 (function (o) {
-	o.XML2OBJECT = function (url, callback, start = "rss") {
-		var that = this,
-			data = $.Deferred();
+
+	o.XML2OBJECT = function (url, callback, start) {
+		var that = this.constructor == o.XML2OBJECT ? this : {},
+			data = $.Deferred().done(function (data) {
+				console.log(data);
+				o.xdata[o.xdata.length] = data;
+				parseData.call(that, $(data).find(startNode));
+				$.extend(o.jdata, that)
+				callback();
+			}),
+			startNode = start !== undefined ? start : ":first";
+		o.jdata = {};
+		o.xdata = [];
 		getData(data, url);
-		$.when(data).done(function (data) {
-			o.xdata = data;
-			parseData.call(that, $(data).find(start));
-			o.jdata = that;
-			callback();
-		})
 	}
 
 	function getData(def, url) {
 		$.ajax({
 			url: url,
-			cache: false
+			cache: false,
+			converters: {
+				"text xml": jQuery.parseXML
+			},
 		}).always(function (dataFeed) {
 			def.resolve(dataFeed);
 		})
@@ -26,36 +33,38 @@ var X2O = X2O || {};
 		var that = this,
 			tempObj = {};
 		$.each(xml, function (key, value) {
+			var hasChildren = $(value).children().length > 0 ? true : false;
 			var index;
 			if (that.hasOwnProperty(value.tagName + "s")) {
 				index = that[value.tagName + "s"].push({});
-				if (value.children.length > 0) {
-					parseData.call(that[value.tagName + "s"][index - 1], value.children);
+				if (hasChildren) {
+					parseData.call(that[value.tagName + "s"][index - 1], $(value).children());
 				} else {
-					that[value.tagName + "s"][index - 1][value.tagName] = value.innerHTML;
+					that[value.tagName + "s"][index - 1][value.tagName] = value.textContent;
 				}
 			} else if (that.hasOwnProperty(value.tagName)) {
 				that[value.tagName + "s"] = [];
-				if (value.children.length > 0) {
-					that[value.tagName + "s"].push(Object.assign({}, that[value.tagName]));
+				if (hasChildren) {
+					that[value.tagName + "s"].push($.extend({}, that[value.tagName]));
 					index = that[value.tagName + "s"].push({});
-					parseData.call(that[value.tagName + "s"][index - 1], value.children);
+					parseData.call(that[value.tagName + "s"][index - 1], $(value).children());
 				} else {
 					tempObj[value.tagName] = that[value.tagName];
 					that[value.tagName + "s"].push(tempObj);
 					tempObj = {};
-					tempObj[value.tagName] = value.innerHTML;
+					tempObj[value.tagName] = value.textContent;
 					that[value.tagName + "s"].push(tempObj);
 				}
 				delete that[value.tagName];
 			} else {
-				if (value.children.length > 0) {
+				if (hasChildren) {
 					that[value.tagName] = {}
-					parseData.call(that[value.tagName], value.children);
+					parseData.call(that[value.tagName], $(value).children());
 				} else {
-					that[value.tagName] = value.innerHTML;
+					that[value.tagName] = value.textContent;
 				}
 			}
 		})
 	}
+
 }(X2O))
